109e14e144008e5c0028523f48b5f3567ee71902
anjanpaul <anjanpaul2lrp@gmail.com>
2024-11-20 15:48:15 +0600

    add script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
